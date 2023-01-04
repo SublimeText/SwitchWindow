23f5ef625329f70a68ad68e503316394e817e914
deathaxe <deathaxe82@googlemail.com>
2023-01-04 18:52:57 +0100

    Fix kind icons
    Unicode symbols look weird and broken when rendered italic by
    Default theme. So use normal chars.

--- switch_window.py
+++ switch_window.py
@@ -15,8 +15,8 @@ class WindowInputHandler(sublime_plugin.ListInputHandler):
     def list_items(self):
         items = []
 
-        kind_project = [sublime.KIND_ID_NAMESPACE, "🗔", "Project"]
-        kind_file = [sublime.KIND_ID_NAVIGATION, "🗔", "File"]
+        kind_project = [sublime.KIND_ID_NAMESPACE, "P", "Project"]
+        kind_file = [sublime.KIND_ID_NAVIGATION, "F", "File"]
 
         for window in sublime.windows():
             active_file_name = "untitled"
@@ -30,13 +30,13 @@ class WindowInputHandler(sublime_plugin.ListInputHandler):
 
             project_file_name = window.project_file_name()
             if project_file_name:
-                title = f"Project: {os.path.splitext(os.path.basename(project_file_name))[0]}"
+                title = os.path.splitext(os.path.basename(project_file_name))[0]
                 kind = kind_project
                 details = [
                     f"Active File: {active_file_name}"
                 ]
             else:
-                title = f"File: {active_file_name}"
+                title = active_file_name
                 kind = kind_file
                 details = [
                     f"Project: none"
